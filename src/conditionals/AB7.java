37a8d53c47f50452115f73ce69ac399f4e030558
infreeJ <kasious97@naver.com>
2025-07-11 10:03:43 +0900

    A+B - 7(11021번)

---
+++ src/conditionals/AB7.java
@@ -0,0 +1,34 @@
+package conditionals;
+
+import java.io.*;
+
+public class AB7 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        try {
+            int count = Integer.parseInt(br.readLine());
+            for (int i = 0; i < count; i++) {
+                String[] input = br.readLine().split(" ");
+                int a = Integer.parseInt(input[0]);
+                int b = Integer.parseInt(input[1]);
+                String strInt = String.valueOf(a + b);
+                int iCnt = i+1;
+                bw.write("Case #" + iCnt + ": " + strInt);
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+                if (bw != null) bw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
