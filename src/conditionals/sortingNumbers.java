677574e887855c67afd5d21acddc4421d5bbdd23
infreeJ <kasious97@naver.com>
2025-07-12 15:52:52 +0900

    수 정렬하기(2750번)

---
+++ src/conditionals/sortingNumbers.java
@@ -0,0 +1,29 @@
+package conditionals;
+
+import java.util.Scanner;
+
+public class sortingNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-1; j++) {
+                if(num[j] > num[j+1] ) {
+                    int tmp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
